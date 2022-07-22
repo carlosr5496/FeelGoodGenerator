f11d143c804067d6839725ead9b2468d81c3e294
Ryan Carlos <ryancarlos925@gmail.com>
2022-07-22 13:54:19 -0700

    First try gathering jokes from API

---
+++ index.js
@@ -0,0 +1,27 @@
+let apiData = fetch("https://v2.jokeapi.dev/joke/Any"
+// , {
+//     method: 'GET',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//     },
+//     body: JSON.stringify({
+//     })
+//}
+
+) .then(res => {
+    if(res.ok) {
+        console.log('SUCCESS')
+    } else {
+        console.log("not successful")
+    }
+    res.json()
+})
+.then(data => JSON.stringify(data))
+.catch(error => console.log("ERROR"));
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('DOM LOADED')
+})
