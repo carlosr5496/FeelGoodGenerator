52e4990f269f1e573ebeaf31299fc80079977b22
Ryan Carlos <ryancarlos925@gmail.com>
2022-07-23 13:31:34 -0700

    Successfully added joke data from API to append in h3

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-document.addEventListener("click", () => console.log('JokeBtn clicked!'))
 
-const genJoke = (e) => {
+// document.querySelector('.newJoke').addEventListener('click', () => alert('clicked'))
+// const appendJoke = () => {
+//     let h3 = document.querySelector('h3');
+//     let newJoke = document.createElement('p');
+//     h3.append(newJoke)
+//     newJoke.innerText = targetApiJoke();
+// }
+
+
+const targetJokeBtn = document.querySelector('.newJoke');
+targetJokeBtn.addEventListener('click', () => {
+
+    console.log('i was clicked');
+    return targetApiJoke();
+})
+
+
+
+const targetApiJoke = () => {
+    fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist&type=single')
     
-}
+    .then(res => res.json())
+    
+    .then(data => {
+        JSON.stringify(data);
+       let copyJoke = data.joke;
+       let h3 = document.querySelector('h3');
+       let newJoke = document.createElement('p');
+       h3.append(newJoke)
+       newJoke.innerText = copyJoke;
+       
+       console.log(copyJoke);
+       
+    })
+    
+    .catch (error => console.log('ERROR'))};
 
-let apiData = fetch("https://v2.jokeapi.dev/joke/Any"
-// , {
-//     method: 'GET',
-//     headers: {
-//         'Content-Type': 'application/json',
-//         'Accept': 'application/json'
-//     },
-//     body: JSON.stringify({
-//     })
-//}
-
-) .then(res => {
-    if(res.ok) {
-        console.log('SUCCESS')
-    } else {
-        console.log("not successful")
-    }
-    res.json()
-})
-.then(data => JSON.stringify(data))
-.catch(error => console.log("ERROR"));
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', function(){
-    console.log('DOM LOADED')
-})
+document.addEventListener('DOMContentLoaded', () => console.log('dom loaded'))
