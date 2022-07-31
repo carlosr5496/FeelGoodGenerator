4c31541b5e7e1f491c78a2b87c11aeda90e1344f
Ryan Carlos <ryancarlos925@gmail.com>
2022-07-30 19:36:52 -0700

    Added new btns, also changed CSS styling

--- apiScript.js
+++
@@ -1,5 +0,0 @@
-console.log(fetch("https://v2.jokeapi.dev/joke"))
-
-document.addEventListener("DOMContentLoaded", function() {
-    console.log('DOM has loaded.');
-});
--- index.js
+++ index.js
@@ -1,39 +1,107 @@
 
-// document.querySelector('.newJoke').addEventListener('click', () => alert('clicked'))
-// const appendJoke = () => {
-//     let h3 = document.querySelector('h3');
-//     let newJoke = document.createElement('p');
-//     h3.append(newJoke)
-//     newJoke.innerText = targetApiJoke();
-// }
+//Fetch joke and extract only the joke to append to DOM
+
+const targetApiJoke = () => {
+    fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,explicit,racist,sexist&type=single')
+
+        .then(res => res.json())
+
+        .then(data => {
+            JSON.stringify(data);
+            let copyJoke = data.joke;
+            let h3 = document.querySelector('h3');
+            let newJoke = document.createElement('p');
+            h3.append(newJoke);
+            newJoke.innerText = copyJoke;
+
+            console.log(copyJoke);
+        })
+
+        .catch(error => console.log(error))
+};
 
 
-const targetJokeBtn = document.querySelector('.newJoke');
-targetJokeBtn.addEventListener('click', () => {
+// const targetNsfwJoke = () => {
+const targetNsfwJoke = function () {
+    fetch('https://v2.jokeapi.dev/joke/Dark?blacklistFlags=religious,political,racist,sexist&type=single')
 
-    console.log('i was clicked');
+        .then(res => res.json())
+
+        .then(data => {
+            JSON.stringify(data);
+            let nsfwJoke = data.joke;
+            let h4 = document.querySelector('h4');
+            let newJoke = document.createElement('p');
+            let deleteBtn = document.createElement('button');
+            deleteBtn.innerText = 'X';
+            h4.append(newJoke);
+            newJoke.innerText = nsfwJoke ;
+            h4.insertAdjacentElement('beforeend', deleteBtn);
+
+            console.log(nsfwJoke);
+        })
+
+        .catch(error => console.log(error))
+};
+
+const memeAPI = function() {
+    fetch('https://api.thecatapi.com/v1/images/search')
+    .then(resp => resp.json())
+    .then(data => {
+        const image = document.querySelector('img');
+        //destructure json array, taking out the URL variable
+        /* [{
+            "id": "6r6",
+            "url": "https://cdn2.thecatapi.com/images/6r6.jpg",
+            "width": 2736,
+            "height": 3648
+            }]
+         */
+        const {url} = data[0];
+        // const url = data[0].url;
+
+
+        console.log(url);
+        image.src = url;
+        // image.innerText = data.url;
+    })
+}
+
+
+//Create delete button with click event
+
+
+
+const handleDelete = () => {
+    deleteBtn.addEventListener('click', () => {
+
+    })
+}
+
+// Variable handling the Joke button with click event
+const safeJokeBtn = document.querySelector('#safeJoke');
+safeJokeBtn.addEventListener('click', () => {
+
+    //Once clicked, fetch to the Joke API will occur
+    console.log('i clicked safe joke');
     return targetApiJoke();
 })
 
+const nsfwBtn = document.querySelector('.nsfwJoke');
+nsfwBtn.addEventListener('click', () => {
+    console.log('I clicked nsfw')
+    return targetNsfwJoke();
+})
+
+const catImg = document.querySelector('.catBtn');
+catImg.addEventListener('click', () =>{
+    return memeAPI();
+})
 
 
-const targetApiJoke = () => {
-    fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist&type=single')
-    
-    .then(res => res.json())
-    
-    .then(data => {
-        JSON.stringify(data);
-       let copyJoke = data.joke;
-       let h3 = document.querySelector('h3');
-       let newJoke = document.createElement('p');
-       h3.append(newJoke)
-       newJoke.innerText = copyJoke;
-       
-       console.log(copyJoke);
-       
-    })
-    
-    .catch (error => console.log('ERROR'))};
+// NSFW api
 
 document.addEventListener('DOMContentLoaded', () => console.log('dom loaded'))
+
+
+
